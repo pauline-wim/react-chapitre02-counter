9b3f49ca8ee658ead5af19564493c5fa6bfb27c3
pauline-wim <linette.wim@hotmail.com>
2022-01-25 15:02:00 +0100

    01 - Compteur

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// CSS
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function decrementCounter(previousState) {
+  return {
+      count: previousState.count - 1
+  };
+};
+
+function incrementCounter(previousState) {
+  return {
+      count: previousState.count + 1
+  };
+};
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div className="main">
+
+        <h1>Counter</h1>
+
+        <div className="container">
+
+          <button className="btn red" onClick={() => {
+							this.setState(decrementCounter);
+						}}>-</button>
+
+          <h2>{this.state.count}</h2>
+          
+          <button className="btn green" onClick={() => {
+							this.setState(incrementCounter);
+						}}>+</button>
+
+        </div>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
